ab976731bbfa0d9a35a1ec47aa441e68b3cbcc32
k2supra <andrijkorolevic@gmail.com>
2022-10-29 11:13:11 +0300

    Created new lab4

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("dev")
+def ple(number):
+    return number ** 3
+
+
+try:
+    print("Введіть число")
+    num = int(input("->"))
+
+
+
+
+    result = ple(num)
+    print(f"result = {result}")
+
+except Exception as ex:
+    print(f'Error', {ex})
