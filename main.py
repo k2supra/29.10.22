e9ced741190d733d04ea6a5f9b85229a7165a421
k2supra <andrijkorolevic@gmail.com>
2022-10-29 11:41:21 +0300

    Created new lab6

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("dev")
+def numbr(number):
+    if number >= 0:
+        return 'True'
+
+    else:
+        return 'False'
+
+try:
+    print("Enter a number")
+    n1 = int(input("->"))
+
+
+
+
+except Exception as ex:
+    print(f'Error', {ex})
