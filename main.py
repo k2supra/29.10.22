8d951c17fe97d7d1eb64f302d07331a69303b571
k2supra <andrijkorolevic@gmail.com>
2022-10-29 11:03:29 +0300

    Created new lab3

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("dev")
+def cili(n1):
+    if n1 % 1 == 0 and n1 % n1 == 0:
+        print(f"{n1}, цілі")
+    else:
+        print("Не цілі")
+
+
+try:
+    print("Введіть число")
+    n1 = int(input("->"))
+    def cili(n1):
+        if n1 % 1 == 0 and n1 % n1 == 0:
+            print(f"{n1}, цілі")
+        else:
+            print("Не цілі")
+
+    def main():
+        cili(n1)
+
+    main()
+
+
+
+except Exception as ex:
+    print(f'Error', {ex})
