1cc297a6ddfe44439f3308185a9cd2acabedab62
k2supra <andrijkorolevic@gmail.com>
2022-10-29 09:50:27 +0300

    Created new lab1

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("dev")
+try:
+    a1 = int(input("->"))
+    b1 = int(input("->"))
+
+    def print_rec(a, b):
+        for i in range(0, a):
+            for j in range(0, b):
+                print("/", end=" ")
+            print()
+        print()
+
+
+    def main():
+        print_rec(a1, b1)
+
+
+    main()
+
+except Exception as ex:
+    print(f'Error', {ex})
