088eba6bba494f8c1102c644f48a6c4f5dd6719e
k2supra <andrijkorolevic@gmail.com>
2022-10-29 10:23:51 +0300

    Created new lab2

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("dev")
+try:
+    a1 = int(input("->"))
+
+    def fact(a):
+        f = 1
+        for i in range(1, a1 + 1):
+            f = f * i
+        print(f)
+    def main():
+        fact(a1)
+
+    main()
+
+
+except Exception as ex:
+    print(f'Error', {ex})
